1722fcd14132348db4da9970211a737a7d076403
SergioCalde <sergio.calderon.dm@outlook.com>
2022-09-18 22:33:50 -0600

    Add marker, popup and navigate to initial position

--- src/main.ts
+++ src/main.ts
@@ -4,15 +4,20 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+import Mapboxgl from 'mapbox-gl'; // or "const mapboxgl = require('mapbox-gl');"
 
-if( !navigator.geolocation ) {
-  alert('Navegador no soporta la Geolocation')
-  throw new Error('Navegador no soporta la Geolocation')
+Mapboxgl.accessToken =
+  'pk.eyJ1Ijoic2NkbTk3IiwiYSI6ImNsNnp1NDA3ZDAwdzAzdm5uc2VicTMwcmUifQ.gPVuIBJpSrJwQ-atHZquzg';
+
+if (!navigator.geolocation) {
+  alert('Navegador no soporta la Geolocation');
+  throw new Error('Navegador no soporta la Geolocation');
 }
 
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+platformBrowserDynamic()
+  .bootstrapModule(AppModule)
+  .catch((err) => console.error(err));
